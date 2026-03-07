4232519a7666920dff32dc1433738517a418ef0b
Raphael Boidol <boidolr@users.noreply.github.com>
2026-03-07 19:05:59 +0100

    infra: extend tests to run on different os

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,13 @@
+import os
+import sys
+
+import pytest
+
 pytest_plugins = "pytester"
+
+
+def pytest_configure(config: pytest.Config) -> None:
+    # provide fallback for Windows tmp_dir implementation of `pytest`
+    if pytest.version_tuple[0] in (7, "7") and sys.platform == "win32":
+        if not any(os.environ.get(n) for n in ("LOGNAME", "USER", "LNAME", "USERNAME")):
+            os.environ["USERNAME"] = "tester"
